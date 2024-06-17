c71760eebc74166ef9e0aa2d8ecea5ee5e8b7fb6
youssef-ala <yousefalaa202@gmail.com>
2024-06-17 07:24:16 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+<StarsCanvas/>
+<Navbar/>
+        {children}
+      </body>
     </html>
   );
 }
